913f1d253296fd4284c40145f4d665b60edb9b84
SUPHAL-MAITY <suphalrohit@gmail.com>
2025-07-21 22:08:57 +0530

    button in add restaurant

--- src/components/Layout/AddRestro.jsx
+++ src/components/Layout/AddRestro.jsx
@@ -257,14 +257,17 @@ const AddRestro = () => {
             Location
           </label>
         </div>
-
-        <button
+        <div className="flex justify-center">
+          <button
           type="submit"
           disabled={!auth.token}
-          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          className="text-white mb-4  bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
-          Submit
+          Submits
         </button>
+
+        </div>
+        
       </form>
     </div>
   );
