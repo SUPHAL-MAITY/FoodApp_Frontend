a1a61616384021fa696d6407215aaf898641d1d6
SUPHAL-MAITY <suphalrohit@gmail.com>
2025-07-21 18:29:49 +0530

    sentance added

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,7 +1,7 @@
 const Loader = () => {
   return (
     <>
-      <div className="flex justify-center items-center h-20 w-full">
+      <div className="flex flex-col justify-center items-center h-20 w-full">
         <div className="w-10 h-10 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
         <h1> It is on the free server , so it might take time first time</h1>
       </div>
