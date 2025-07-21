53be13b4eeec4c0592278a5772f263069bea99e8
SUPHAL-MAITY <suphalrohit@gmail.com>
2025-07-21 18:32:13 +0530

    red-text  color addded

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -3,7 +3,7 @@ const Loader = () => {
     <>
       <div className="flex flex-col justify-center items-center h-20 w-full">
         <div className="w-10 h-10 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
-        <h1> It is on the free server , so it might take time first time</h1>
+        <h1 className="text-red-500"> It is on the free server , so it might take some time  for first time </h1>
       </div>
     </>
   );
