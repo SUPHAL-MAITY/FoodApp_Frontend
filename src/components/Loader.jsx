7f3c57459b378e08b2acb276faf821dc1d7977a9
SUPHAL-MAITY <suphalrohit@gmail.com>
2025-07-21 18:28:03 +0530

    sentence added in loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -3,6 +3,7 @@ const Loader = () => {
     <>
       <div className="flex justify-center items-center h-20 w-full">
         <div className="w-10 h-10 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
+        <h1> It is on the free server , so it might take time first time</h1>
       </div>
     </>
   );
